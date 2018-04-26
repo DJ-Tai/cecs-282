c8509a528917b416ddd33378aee15fe52ba60339
David T <david.jt@protonmail.com>
2018-04-26 15:29:07 -0700

    Testing a Merge
    Testing 1,2,3...

---
+++ final_proj/test_folder/test_src/Store.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : Store.cpp
+// Author      : David Taitingfong
+// Date        : Apr 16, 2018
+// Version     :
+// Copyright   : 
+// Description :
+//============================================================================
+
+#include "Store.h"
+
+/**
+ * Default constuctor
+ */
+Store::Store()
+{
+	this->name = "";
+	this->address = "";
+	this->phone_num = "";
+	this->fax_num = "";
+}
+
+Store::Store(string n, string a, string pn, string fn)
+{
+
+}
+
