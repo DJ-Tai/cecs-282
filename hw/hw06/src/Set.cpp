3f2d316582472bdecf9e31ad5fc3b66d7d705b49
David T <david.jt@protonmail.com>
2018-05-02 10:57:59 -0700

    Start Homework 6, Question 2
    See title

---
+++ hw/hw06/src/Set.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : Set.cpp
+// Author      : david
+// Date        : May 2, 2018
+// Version     :
+// Copyright   : 
+// Description :
+//============================================================================
+
+#include "Set.h"
+
+Set::Set()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Set::~Set()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ hw/hw06/src/Set.h
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Set.h
+// Author      : david
+// Date        : May 2, 2018
+// Version     :
+// Copyright   : 
+// Description :
+//============================================================================
+
+#ifndef SET_H_
+#define SET_H_
+
+class Set
+{
+public:
+	Set();
+	virtual ~Set();
+};
+
+#endif /* SET_H_ */
